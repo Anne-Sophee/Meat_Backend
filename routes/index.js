a1e3d9ee7b2efbc88cf9e738810d933d9b1489ea
Anne-Sophee <anesofie.menuel@gmail.com>
2022-03-04 13:55:47 +0100

    test11

--- routes/index.js
+++ routes/index.js
@@ -22,12 +22,17 @@ router.post('/login', async function(req, res, next) {
   //recherche du document correspondant à l'email reçu du frontend
   var user = await UserModel.findOne({email});
 
+  if (user) {
   //comparaison des mdp crytés pour permettre le login
     if (bcrypt.compareSync(password, user.password)) {
       res.json({ login: true, user });
     } else {
       res.json({ login: false, errorPassword });
     }
+
+  } else {
+    res.json({ error, user })
+  }
 })
 
 

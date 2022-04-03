cc5c81157691b030dc2aac8b189a40826a56d4f9
Anne-Sophee <anesofie.menuel@gmail.com>
2022-04-03 03:57:56 +0200

    test84

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var interactionsRouter = require('./routes/interactions');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/interactions', interactionsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
